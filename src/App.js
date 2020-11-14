754e82d86ce23f78a6f346bfc7208273cd7c4ab5
unknown <rabeet.k1@gmailcom>
2020-11-14 23:00:00 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import QuizLayout from "./QuizLayout/QuizLayout";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <QuizLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/QuizLayout/ProgressBar.js
@@ -0,0 +1,8 @@
+import { Progress } from "react-sweet-progress";
+import "react-sweet-progress/lib/style.css";
+
+const ProgressBar = (props) => {
+  return <Progress percent={20} />;
+};
+
+export default ProgressBar;
